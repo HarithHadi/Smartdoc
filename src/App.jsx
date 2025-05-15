c60b7912f094d97d08748b8b84050c573544dc24
Fizzo43 <fizohafizj@gmail.com>
2025-05-15 10:28:20 +0800

    Update v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 
-//saya suka
+//App.jsx
 import { useState } from "react";
 import "./App.css";
 import Chat from "./components/chat";
 import GrChat from "./components/grchat";
-
 
 
 function App() {
